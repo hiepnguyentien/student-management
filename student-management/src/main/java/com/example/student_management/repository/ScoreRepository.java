76f2f0ccb1e0d28bc75a07a3a362361d397f3507
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-07-12 17:04:48 +0700

    API điểm sinh viên

--- student-management/src/main/java/com/example/student_management/repository/ScoreRepository.java
+++ student-management/src/main/java/com/example/student_management/repository/ScoreRepository.java
@@ -1,5 +1,12 @@
 package com.example.student_management.repository;
 
-public class ScoreRepository {
-    
+import com.example.student_management.model.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScoreRepository extends JpaRepository<Score, Long> {
+    @Query("SELECT s FROM Score s WHERE s.student.id = ?1 AND s.subjectClass.id = ?2")
+    Score findByStudentIdAndSubjectClassId(Long studentId, Long subjectClassId);
 }
