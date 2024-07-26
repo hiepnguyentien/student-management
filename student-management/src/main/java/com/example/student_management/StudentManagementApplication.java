4f6f2ab3e707f2a785205146a68ba2e8c426fc24
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-07-26 10:38:27 +0700

    ...

--- student-management/src/main/java/com/example/student_management/StudentManagementApplication.java
+++ student-management/src/main/java/com/example/student_management/StudentManagementApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementApplication {
+	//test
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
