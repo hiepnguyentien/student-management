b0443aaea0939f79e93d800725cd488dd1c21019
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-07-30 17:38:36 +0700

    chore: Update SecurityConfig with CustomJwtDecoder

--- student-management/src/main/java/com/example/student_management/configuration/SecurityConfig.java
+++ student-management/src/main/java/com/example/student_management/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.student_management.configuration;
 
 import com.example.student_management.enums.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,10 +26,12 @@ import javax.crypto.spec.SecretKeySpec;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final String[] PUBLIC_ENDPOINTS = {"/student", "/auth/token", "/auth/introspect", "/lecturer/auth/token"};
+    private final String[] PUBLIC_ENDPOINTS = {"/student", "/auth/token",
+            "/auth/introspect", "/lecturer/auth/token", "/lecturer/auth/logout",
+            "/lecturer/auth/refresh"};
 
-    @Value("${jwt.signerKey}")
-    private String signerKey;
+    @Autowired
+    private CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -38,7 +41,7 @@ public class SecurityConfig {
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
           oauth2.jwt(jwtConfigurer ->
-                  jwtConfigurer.decoder(jwtDecoder())
+                  jwtConfigurer.decoder(customJwtDecoder)
                           .jwtAuthenticationConverter(jwtAuthenticationConverter()))
                   .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
         );
@@ -57,15 +60,6 @@ public class SecurityConfig {
         return jwtAuthenticationConverter;
     }
 
-    @Bean
-    JwtDecoder jwtDecoder() {
-        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
-
-        return NimbusJwtDecoder
-                .withSecretKey(secretKeySpec)
-                .macAlgorithm(MacAlgorithm.HS512)
-                .build();
-    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
