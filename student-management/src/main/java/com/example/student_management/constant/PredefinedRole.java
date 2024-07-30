494dab75b3e519799430e17756d6f1a40b272f43
Nguyễn Tiến Hiệp <101116629+hiepnguyentien@users.noreply.github.com>
2024-07-30 17:39:20 +0700

    chore: Add PredefinedRole class for predefined role constants

---
+++ student-management/src/main/java/com/example/student_management/constant/PredefinedRole.java
@@ -0,0 +1,9 @@
+package com.example.student_management.constant;
+
+public class PredefinedRole {
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String LECTURER_ROLE = "LECTURER";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+    private PredefinedRole() {}
+}
